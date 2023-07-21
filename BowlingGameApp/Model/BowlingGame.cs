d4f004311b46059cd88d5e0fb704c27f8950e38f
BluFeather <BluFeather@protonmail.com>
2023-07-20 22:39:10 -0700

    Pass ZeroPoints_IfGutterGame

---
+++ BowlingGameApp/Model/BowlingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BowlingGameApp.Model
+{
+    public class BowlingGame
+    {
+        public void Roll(int value)
+        {
+            
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
--- BowlingGameAppTests/BowlingGameTests.cs
+++ BowlingGameAppTests/BowlingGameTests.cs
@@ -1,11 +1,19 @@
+using BowlingGameApp.Model;
+
 namespace BowlingGameAppTests
 {
     public class BowlingGameTests
     {
         [Fact]
-        public void Test1()
+        public void ZeroPoints_IfGutterGame()
         {
+            var game = new BowlingGame();
+            for (var roll = 0; roll < 20; roll++)
+            {
+                game.Roll(0);
+            }
 
+            Assert.Equal(0, game.Score());
         }
     }
 }
