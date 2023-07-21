4663836af3e05fced6ca1e3f46d1f437eab4eec5
BluFeather <BluFeather@protonmail.com>
2023-07-20 22:42:05 -0700

    Pass TwentyPoints_IfOnesGame

--- BowlingGameApp/Model/BowlingGame.cs
+++ BowlingGameApp/Model/BowlingGame.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace BowlingGameApp.Model
 {
     public class BowlingGame
     {
+        List<int> Rolls = new List<int>();
+
         public void Roll(int value)
         {
-            
+            Rolls.Add(value);
         }
 
         public int Score()
         {
-            return 0;
+            return Rolls.Sum();
         }
     }
 }
--- BowlingGameAppTests/BowlingGameTests.cs
+++ BowlingGameAppTests/BowlingGameTests.cs
@@ -15,5 +15,17 @@ namespace BowlingGameAppTests
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        public void TwentyPoints_IfOnesGame()
+        {
+            var game = new BowlingGame();
+            for (var roll = 0; roll < 20; roll++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.Equal(20, game.Score());
+        }
     }
 }
