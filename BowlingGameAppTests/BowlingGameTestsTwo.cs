9325bfde5d7ab0d566d78238d0580539a5912d2d
BluFeather <BluFeather@protonmail.com>
2023-07-23 15:53:35 -0700

    Pass ExpectedValuesIfGutterGame.

--- BowlingGameAppTests/BowlingGameTestsTwo.cs
+++ BowlingGameAppTests/BowlingGameTestsTwo.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BowlingGameApp.Model;
+using Xunit.Abstractions;
 
 namespace BowlingGameAppTests
 {
-    internal class BowlingGameTestsTwo
+    public class BowlingGameTestsTwo
     {
+        public BowlingGameTestsTwo(ITestOutputHelper output)
+        {
+            Output = output;
+            Game = new BowlingGame();
+        }
+
+        public ITestOutputHelper Output { get; }
+
+        public BowlingGame Game { get; }
+
+        #region Expected Value Tests
+        [Fact]
+        public void ExpectedValuesIfGutterGame()
+        {
+            RollMany(0, 20);
+            var rolls = GetRolls();
+
+            Assert.Equal(20, rolls.Count);
+            
+            var frames = GetFrames();
+            for (int frame = 0; frame < 10; frame++)
+            {
+                Assert.Equal(2, frames[frame].Scores.Count);
+                for (int roll = 0; roll < 2; roll++)
+                {
+                    Assert.Equal(0, frames[frame].Scores[roll]);
+                }
+            }
+        }
+        #endregion
+
+        private void Roll(int pointsEachRoll)
+        {
+            Game.AddRoll(pointsEachRoll);
+        }
+
+        private void RollMany(int pointsEachRoll, int numberOfRolls)
+        {
+            for (int roll = 0; roll < numberOfRolls; roll++)
+            {
+                Roll(pointsEachRoll);
+            }
+        }
+
+        private List<int> GetRolls() => Game.PlayedRolls;
+
+        private List<Frame> GetFrames() => Game.Frames;
     }
 }
