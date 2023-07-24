face0891ed78655a2a3932dc03351cd044f47af6
BluFeather <BluFeather@protonmail.com>
2023-07-24 00:06:54 -0700

    Expose Frame Properties.

--- BowlingGameApp/View/UserControls/FrameUserControl.xaml.cs
+++ BowlingGameApp/View/UserControls/FrameUserControl.xaml.cs
@@ -17,6 +17,31 @@ namespace BowlingGameApp.View.UserControls
             set => SetValue(FrameViewModelProperty, value);
         }
 
+        public string FrameNumber
+        {
+            get => Frame.FrameNumber;
+        }
+
+        public string RollOne
+        {
+            get => Frame.RollOne;
+        }
+
+        public string RollTwo
+        {
+            get => Frame.RollTwo;
+        }
+
+        public string RollThree
+        {
+            get => Frame.RollThree;
+        }
+
+        public string RunningScore
+        {
+            get => Frame.RunningScore;
+        }
+
         public static readonly DependencyProperty FrameViewModelProperty =
             DependencyProperty.Register(
                 name: "Frame",
