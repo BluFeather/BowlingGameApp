0939ce89aa857be6d34de0a23f111c2d36ca8470
BluFeather <BluFeather@protonmail.com>
2023-07-24 00:39:53 -0700

    Add Game and Frames to MainWindow.

--- BowlingGameApp/View/MainWindow.xaml.cs
+++ BowlingGameApp/View/MainWindow.xaml.cs
@@ -1,28 +1,32 @@
-﻿using System;
+﻿using BowlingGameApp.Model;
+using BowlingGameApp.ViewModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BowlingGameApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Game = BowlingGameInstance.GameInstance;
+        }
+
+        public BowlingGame Game { get; }
+
+        public List<FrameViewModel> Frames
+        {
+            get
+            {
+                List<FrameViewModel> frames = new List<FrameViewModel>();
+                foreach (var frame in Game.Frames)
+                {
+                    frames.Add(new FrameViewModel(frame));
+                }
+                return frames;
+            }
         }
     }
 }
