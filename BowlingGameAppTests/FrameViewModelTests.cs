bcd9d22f01a8f4e58707ad79aa8a2f4535890652
BluFeather <BluFeather@protonmail.com>
2023-07-23 22:11:33 -0700

    Add FrameViewModel and Test.

---
+++ BowlingGameAppTests/FrameViewModelTests.cs
@@ -0,0 +1,18 @@
+﻿using BowlingGameApp.ViewModel;
+using Xunit.Abstractions;
+
+namespace BowlingGameAppTests
+{
+    public class FrameViewModelTests
+    {
+        public FrameViewModelTests(ITestOutputHelper output)
+        {
+            Output = output;
+            Frame = new FrameViewModel();
+        }
+
+        public ITestOutputHelper Output { get; }
+
+        public FrameViewModel Frame { get; }
+    }
+}
