7fb219319e603f2f80a8d7301d9260536d641d67
BluFeather <BluFeather@protonmail.com>
2023-07-24 00:05:21 -0700

    Add Frame DependencyProperty.

--- BowlingGameApp/View/UserControls/FrameUserControl.xaml.cs
+++ BowlingGameApp/View/UserControls/FrameUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using BowlingGameApp.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace BowlingGameApp.View.UserControls
 {
@@ -8,5 +10,18 @@ namespace BowlingGameApp.View.UserControls
         {
             InitializeComponent();
         }
+
+        public FrameViewModel Frame
+        {
+            get => (FrameViewModel)GetValue(FrameViewModelProperty);
+            set => SetValue(FrameViewModelProperty, value);
+        }
+
+        public static readonly DependencyProperty FrameViewModelProperty =
+            DependencyProperty.Register(
+                name: "Frame",
+                propertyType: typeof(FrameViewModel),
+                ownerType: typeof(FrameUserControl),
+                typeMetadata: new FrameworkPropertyMetadata(typeof(FrameViewModel)));
     }
 }
